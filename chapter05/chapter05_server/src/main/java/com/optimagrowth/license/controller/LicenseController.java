47c4ecece5394401de79b3dfe49bcff37a9ec5ed
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-07-26 15:39:22 +0900

    Connect DS using spring cloud config server

--- chapter05/chapter05_server/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ chapter05/chapter05_server/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -43,18 +43,18 @@ public class LicenseController {
 	}
 
 	@PutMapping
-	public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
-		return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+	public ResponseEntity<License> updateLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
+		return ResponseEntity.ok(licenseService.updateLicense(request));
 	}
 
 	@PostMapping
-	public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request,
+	public ResponseEntity<License> createLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request,
 			@RequestHeader(value = "Accept-Language",required = false) Locale locale) {
-		return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+		return ResponseEntity.ok(licenseService.createLicense(request));
 	}
 
 	@DeleteMapping(value="/{licenseId}")
 	public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
-		return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+		return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
 	}
 }
