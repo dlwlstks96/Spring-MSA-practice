a73a2b3b40cabe63c156cf4dead72e0730d066ce
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-08-02 16:40:42 +0900

    Add config setting(yml) to use Eureka server

--- chapter06/eurekaserver/src/main/java/com/optimagrowth/eureka/EurekaServerApplication.java
+++ chapter06/eurekaserver/src/main/java/com/optimagrowth/eureka/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.optimagrowth.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer //스프링 서비스에서 유레카 서버를 활성화한다.
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
